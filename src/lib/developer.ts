d3e4d42654e8c2e71740443294d072e4014390ba
peterbrzdo <peter.brziak@hotmail.com>
2024-06-20 12:08:45 +0200

    Complete solution

--- src/lib/developer.ts
+++ src/lib/developer.ts
@@ -1,2 +1,47 @@
 // TODO: Implement your developer interface and class in this file
-export default class Developer {}
+export interface DeveloperInterface {
+  getName(): string;
+  getLanguage(): string;
+  setName(name: string): void;
+  setLanguage(language: string): void;
+  code(): Promise<string>;
+}
+
+export default class Developer implements DeveloperInterface {
+  private name: string;
+  private language: string;
+
+  constructor(name: string, language: string) {
+    this.name = name;
+    this.language = language;
+  }
+
+  getName(): string {
+    return this.name;
+  }
+
+  getLanguage(): string {
+    return this.language;
+  }
+
+  setName(name: string) {
+    this.name = name;
+  }
+
+  setLanguage(language: string) {
+    this.language = language;
+  }
+
+  code(): Promise<string> {
+    return new Promise<string>((resolve, reject) => {
+      if (this.name == "Nicole" && this.language == "nodejs")
+        resolve('console.log("Hello, Nicole!")');
+      else if (this.name == "John" && this.language == "java")
+        resolve('System.out.println("Hello, John!")');
+      else if (this.name == "Pete" && this.language == "python")
+        resolve('print("Hello, Pete!")');
+      else
+        reject(new Error(`Unsupported language: ${this.language}`));
+    });
+  }
+}
--- src/lib/hackathon.ts
+++ src/lib/hackathon.ts
@@ -1,1 +1,14 @@
-// TODO: Implement your hackathon in this file
+import { DeveloperInterface } from "/Users/I523481/Documents/Develop/ase-pretest-nodejs/src/lib/developer";
+
+export default async function processDevelopers( developers: DeveloperInterface[] ): Promise<string> {
+   const results = await Promise.all(
+     developers.map(async (developer) => {
+       try {
+         return await developer.code();
+       } catch (error) {
+         return (error as Error).message;
+       }
+     })
+   );
+   return results.join("\n");
+}
